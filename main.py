875c2ac93cb2a63413b14172b2fbb4a4b879cbcc
Jesus carrion <jesusalfonsocarrionmendoza23@gmail.com>
2026-06-29 22:28:16 -0400

    feat(main): add hypothesis 1 and 2 outputs

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import pandas as pd
 from src.analysis.shorts_vs_longs import analyze_engagement_by_type
-from src.preprocessing import calculate_engagement_rate
+from src.analysis.hype_decay import calculate_correlations
+from src.preprocessing import calculate_engagement_rate, compute_days_since_published
 
 df = pd.read_csv("data/cristiano_youtube_stats.csv")
 df.info()
 
-analyze_engagement_by_type(calculate_engagement_rate(df)).info()
+# Hypotesis 1
+df = calculate_engagement_rate(df)
+print(analyze_engagement_by_type(df))
+
+# Hypotesis 2
+df = compute_days_since_published(df)
+print(calculate_correlations(df))
