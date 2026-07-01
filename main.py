2f7cb969d33a4c53390671949bbc9fd67415d56f
Jesus carrion <jesusalfonsocarrionmendoza23@gmail.com>
2026-07-01 00:57:26 -0400

    feat(main): add hypothesis 3 output

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from src.analysis.shorts_vs_longs import analyze_engagement_by_type
 from src.analysis.hype_decay import calculate_correlations
+from src.analysis.sweet_spot import analyze_duration_segments
 from src.preprocessing import run_preprocessing_pipeline
 
 df = pd.read_csv("data/cristiano_youtube_stats.csv")
@@ -13,3 +14,6 @@ print(analyze_engagement_by_type(df))
 
 # Hypotesis 2
 print(calculate_correlations(df))
+
+# Hypotesis 3
+print(analyze_duration_segments(df))
