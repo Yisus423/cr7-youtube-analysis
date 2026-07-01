d249dac9fae305c55a4310f53ccf38084d3b95e1
Jesus carrion <jesusalfonsocarrionmendoza23@gmail.com>
2026-06-30 20:07:40 -0400

    refactor(app): extract logic from main into small functions
    Modularize the app for a better readilibity and edition

--- app.py
+++ app.py
@@ -5,20 +5,16 @@ from src.analysis.shorts_vs_longs import analyze_engagement_by_type
 from src.analysis.hype_decay import calculate_correlations
 
 
-def main():
-    st.set_page_config(page_title="CR7 YouTube Analysis", layout="wide")
-    st.title("📊 Análisis de Rendimiento: Canal de Cristiano Ronaldo")
-    # 1. Carga y preprocesamiento (esto se ejecuta cada vez que se recarga la app)
-    # En un entorno profesional, aquí se usaría st.cache_data para no re-procesar todo
-    df = pd.read_csv("data/cristiano_youtube_stats.csv")
-    df = calculate_engagement_rate(df)
-    df = compute_days_since_published(df)
+def render_hypothesis_1(df: pd.DataFrame):
     # 2. Análisis Hipótesis 1: Shorts vs Longs
     st.header("1. ¿Shorts o Videos Largos?")
     summary_h1 = analyze_engagement_by_type(df)
     st.dataframe(summary_h1)
     st.bar_chart(data=summary_h1, x="video_type", y="mean_engagement")
-    # 3. Análisis Hipótesis 2: El Desgaste del Hype
+
+
+def render_hypothesis_2(df: pd.DataFrame):
+    # Análisis Hipótesis 2: El Desgaste del Hype
     st.header("2. ¿Desgaste del Hype?")
     summary_h2 = calculate_correlations(df)
     st.table(summary_h2)  # Tabla para ver coeficientes
@@ -27,6 +23,19 @@ def main():
     st.scatter_chart(
         data=df, x="days_since_published", y="viewCount", color="video_type"
     )
+
+
+def main():
+    st.set_page_config(page_title="CR7 YouTube Analysis", layout="wide")
+    st.title("📊 Análisis de Rendimiento: Canal de Cristiano Ronaldo")
+    # 1. Carga y preprocesamiento (esto se ejecuta cada vez que se recarga la app)
+    # En un entorno profesional, aquí se usaría st.cache_data para no re-procesar todo
+    df = pd.read_csv("data/cristiano_youtube_stats.csv")
+    df = calculate_engagement_rate(df)
+    df = compute_days_since_published(df)
+    # 2.Cargamos las hipothesis:
+    render_hypothesis_1(df)
+    render_hypothesis_2(df)
 
 
 if __name__ == "__main__":
