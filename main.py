5fb49b687394f146be3638dc4ec3f6e1c06e126e
Jesus carrion <jesusalfonsocarrionmendoza23@gmail.com>
2026-07-01 23:27:44 -0400

    chore(main): remove import to preprocesing.py
    this import is innecesary because his function is unused.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pandas as pd
 from src.analysis.shorts_vs_longs import analyze_engagement_by_type
 from src.analysis.hype_decay import calculate_correlations
 from src.analysis.sweet_spot import analyze_duration_segments
-from src.preprocessing import run_preprocessing_pipeline
 
 # Cargamos el dataset preprocesado
 df = pd.read_parquet("data/processed_data.parquet")
